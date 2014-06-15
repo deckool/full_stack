0fc2e6316cf998f5a1fc5084c2a7ef50dca316a5
deck <deckool@gmail.com>
2014-06-15 09:54:41 +0300

    hm

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var _redis = require("redis"),
 redis = _redis.createClient();
 
-var restaurants = '[{"id":0,"name":"Pizza Pronto Restaurant","address":"71A Aripita Avenue Woodbrook","city":"Port of spain","phone":"+xx xxx xxx xxx","best":[{"name":"Pizza Margherita","ingredients":"tomato sauce, mozzarella","price":"9.00 USD"},{"name":"Penne Alla Siciliana","ingredients":"pasta with aubergines, mozzarella and tomato sauce","price":"8.00 USD"}],"pizza":[{"name":"Pizza Prosciuto","ingredients":"one one one","price":"10.00 USD"},{"name":"Pizza Margherita","ingredients":"sos, mozzarella, rosii, busuioc","price":"less"}],"pasta":[{"name":"Spaghetti All Arabiatello","ingredients":"pasta faina","price":"8.00 USD"}]}]';
+var restaurants = '[{"id":0,"name":"Pizza Pronto Restaurant","address":"71A Aripita Avenue Woodbrook","city":"Port of spain","phone":"1","best":[{"name":"Pizza Margherita","ingredients":"tomato sauce, mozzarella","price":"9.00 USD"},{"name":"Penne Alla Siciliana","ingredients":"pasta with aubergines, mozzarella and tomato sauce","price":"8.00 USD"}],"pizza":[{"name":"Pizza Prosciuto","ingredients":"one one one","price":"10.00 USD"},{"name":"Pizza Margherita","ingredients":"sos, mozzarella, rosii, busuioc","price":"less"}],"pasta":[{"name":"Spaghetti All Arabiatello","ingredients":"pasta faina","price":"8.00 USD"}]}]';
 
 redis.select(1, function(err,res){
   // you'll want to check that the select was successful here
@@ -34,16 +34,12 @@ app.get('/rest', function(req, res) {
   });
 });
 
-app.get('/rest/:id', function(req, res) {
+app.get('/rest/:no', function(req, res) {
   redis.get('nodejs', function(error, result) {
       if (error) res.send('Error: '+ error);
       else {
-  if(result.length <= req.params.id || req.params.id < 0) {
-    res.statusCode = 404;
-    return res.send('Error 404: No quote found');
-  }
         json = JSON.parse(result);
-        var q = json[req.params.id];
+        var q = json[req.params.no];
         res.json(q);
       }
   });
